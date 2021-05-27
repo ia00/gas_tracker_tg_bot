2430bea04c2b0cdb08d14e26f37e63f9e5c13da5
ia00 <66488784+ia00@users.noreply.github.com>
2021-05-27 18:39:02 +0400

    Add files via upload

---
+++ check_gas.py
@@ -0,0 +1,17 @@
+from telegram import Update
+from telegram.ext import Updater, CommandHandler, CallbackContext
+import requests
+import json
+
+APIKey = 'GGMG7UQJDG1FF36QK4DSPUJ78M8DCZ6RUH'
+
+u = Updater('1693468140:AAGksGkaKQBjP7MllxOcEjddWcBYNjWkKQ8', use_context=True)#token of the bot
+j = u.job_queue
+def callback_minute(context : CallbackContext):
+	result = requests.get('https://api.etherscan.io/api?module=gastracker&action=gasoracle&apikey=' + APIKey).json()['result']
+	Safe, Propose, Fast = int(result['SafeGasPrice']), int(result['ProposeGasPrice']), int(result['FastGasPrice'])
+	context.bot.send_message(chat_id='-582933990', text='fd')
+
+job_minute = j.run_repeating(callback_minute, interval=60, first=10)
+u.start_polling()
+u.idle()
